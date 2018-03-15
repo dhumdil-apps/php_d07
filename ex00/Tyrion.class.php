467c7a5a9ef6d31394dbf6af2d3f9068da5c9f97
adsabau <sabau.ad.adrian@gmail.com>
2018-03-15 10:39:56 +0200

    [+]Tests + Resolve

---
+++ ex00/Tyrion.class.php
@@ -0,0 +1,8 @@
+<?php
+
+class Tyrion extends Lannister {
+    public function myNameIsTyrionTheImp(){
+        print("My name is Tyrion" . PHP_EOL);
+        return Short;
+    }
+}
---
+++ ex02/Targaryen.class.php
@@ -0,0 +1,16 @@
+<?php
+
+class Targaryen{
+    
+    public function getBurned(){
+        if($this->resistsFire()){
+            return "emerges naked but unharmed";
+        } else {
+            return "burns alive";
+        }
+    }
+
+    public function resistsFire(){
+        return False;
+    }
+}
